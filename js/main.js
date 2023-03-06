b14156408bf47f59eb723bfbf3e62e4195e5fcc8
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-03-06 22:03:25 +0700

    Lets start to make adaptive design
    Lets start to make adaptive design

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 20,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-right',
-      prevEl: '.swiper-button-left',
-    },
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     loop: true,
+//     slidesPerView: 2,
+//     spaceBetween: 20,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-right',
+//       prevEl: '.swiper-button-left',
+//     },
+//   });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
+}
+
+window.initMap = initMap;
