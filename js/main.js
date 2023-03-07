7c46edf42af5d9e189148d9622c9a9b07e6639c0
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-03-07 08:54:55 +0700

    Adaptive was started
    Adaptive was started

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     loop: true,
-//     slidesPerView: 2,
-//     spaceBetween: 20,
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-right',
-//       prevEl: '.swiper-button-left',
-//     },
-//   });
+const menuBtn = document.querySelector('.menu-btn')
+const menuMobile = document.querySelector('.menu-mobile')
 
-let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+menuBtn.addEventListener ('click', () => {
+  menuMobile.classList.toggle('menu-open')
+})
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
   });
-}
 
-window.initMap = initMap;
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// window.initMap = initMap;
